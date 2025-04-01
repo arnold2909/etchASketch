63e3891c4ad1e2a56a10a297d033f1237d247509
Arnold Popliceanu <popliceanu.arnold@gmail.com>
2025-04-01 16:12:58 +0100

    Improved readability of code by making the grid in a function and just calling that funciton

--- main.js
+++ main.js
@@ -4,53 +4,33 @@ const containerDiv = document.querySelector("#containerDiv");
 const gridButton = document.querySelector("#gridSizeButton");
 const gridInput = document.querySelector("#gridSizeInput");
 
+const makeGrid = function () {
+  for (let i = 1; i <= gridSize; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.setAttribute("class", "rows");
+    for (let j = 1; j <= gridSize; j++) {
+      const drawDiv = document.createElement("div");
+      drawDiv.setAttribute("class", "smallDivs");
+      drawDiv.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "red";
+      });
+      rowDiv.appendChild(drawDiv);
+    }
+    containerDiv.appendChild(rowDiv);
+  }
+};
+
 gridButton.addEventListener("click", () => {
   if (gridInput.value > 0) {
     gridSize = gridInput.value;
     gridInput.value = "";
     containerDiv.innerHTML = "";
-    for (let i = 1; i <= gridSize; i++) {
-      const rowDiv = document.createElement("div");
-      rowDiv.setAttribute("class", "rows");
-      for (let j = 1; j <= gridSize; j++) {
-        const drawDiv = document.createElement("div");
-        drawDiv.setAttribute("class", "smallDivs");
-        drawDiv.addEventListener("mouseover", (e) => {
-          e.target.style.backgroundColor = "red";
-        });
-        rowDiv.appendChild(drawDiv);
-      }
-      containerDiv.appendChild(rowDiv);
-    }
+    makeGrid();
   } else {
     gridInput.value = "";
     containerDiv.innerHTML = "";
-    for (let i = 1; i <= gridSize; i++) {
-      const rowDiv = document.createElement("div");
-      rowDiv.setAttribute("class", "rows");
-      for (let j = 1; j <= gridSize; j++) {
-        const drawDiv = document.createElement("div");
-        drawDiv.setAttribute("class", "smallDivs");
-        drawDiv.addEventListener("mouseover", (e) => {
-          e.target.style.backgroundColor = "red";
-        });
-        rowDiv.appendChild(drawDiv);
-      }
-      containerDiv.appendChild(rowDiv);
-    }
+    makeGrid();
   }
 });
 
-for (let i = 1; i <= gridSize; i++) {
-  const rowDiv = document.createElement("div");
-  rowDiv.setAttribute("class", "rows");
-  for (let j = 1; j <= gridSize; j++) {
-    const drawDiv = document.createElement("div");
-    drawDiv.setAttribute("class", "smallDivs");
-    drawDiv.addEventListener("mouseover", (e) => {
-      e.target.style.backgroundColor = "red";
-    });
-    rowDiv.appendChild(drawDiv);
-  }
-  containerDiv.appendChild(rowDiv);
-}
+makeGrid();
