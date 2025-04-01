1ad1d514e68093c548c32ad69234867ff1630f4c
Arnold Popliceanu <popliceanu.arnold@gmail.com>
2025-04-01 12:19:06 +0100

    Made the grid, the event listener not working

---
+++ main.js
@@ -0,0 +1,16 @@
+const containerDiv = document.querySelector("#containerDiv");
+
+for (let i = 1; i <= 16; i++) {
+  const rowDiv = document.createElement("div");
+  rowDiv.setAttribute("class", "rows");
+  for (let j = 1; j <= 16; j++) {
+    let drawDiv = document.createElement("div");
+    drawDiv.setAttribute("class", "smallDivs");
+    drawDiv.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "white";
+      console.log("working");
+    });
+    rowDiv.appendChild(drawDiv.cloneNode(true));
+  }
+  containerDiv.appendChild(rowDiv.cloneNode(true));
+}
